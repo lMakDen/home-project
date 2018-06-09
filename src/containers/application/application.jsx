4fd9c22748e51dda03ef2b9848e0ad28d89b3141
Ivan <MakDen1994@gmail.com>
2018-06-09 21:25:54 +0300

    made makeup

--- src/containers/application/application.jsx
+++ src/containers/application/application.jsx
@@ -34,18 +34,27 @@ class Application extends Component {
         let removed = cryptoListObject.splice(0, counter);
 
         return (
-            <HeaderSection>
-                { removed.map((item, i) =>{
-
-                    return <Row>
-                        <div>{i + 1}</div>
-                        <RowImg src={`https://www.cryptocompare.com${cryptoList.data[item].ImageUrl}`} alt="error"/>
-                        <div>{cryptoList.data[item].CoinName}</div>
-                        <div>{prices[item] && prices[item].USD}</div>
-                    </Row>
-                })}
-                <button onClick={this.handleChangeCounter}>Add</button>
-            </HeaderSection>
+            <div>
+                <HeaderSection>
+                    <Header>
+                        <div>#</div>
+                        <div>Logo</div>
+                        <div>Coin</div>
+                        <div>Price (USD)</div>
+                    </Header>
+                    { removed.map((item, i) =>{
+                        return <Row>
+                            <div>{i + 1}</div>
+                            <div>
+                                <RowImg src={`https://www.cryptocompare.com${cryptoList.data[item].ImageUrl}`} alt="error"/>
+                            </div>
+                            <div>{cryptoList.data[item].CoinName}</div>
+                            <div>{prices[item] && prices[item].USD}</div>
+                        </Row>
+                    })}
+                </HeaderSection>
+                <ShowMoreBtn onClick={this.handleChangeCounter}>Show more</ShowMoreBtn>
+            </div>
         );
     }
 
@@ -65,20 +74,51 @@ class Application extends Component {
     };
 }
 const HeaderSection = styled.div`
-        background: white;
-        min-height: 100vh;
+        background-color: whitesmoke;
+        margin: auto;
+        width: 50%;
         display: flex;
         justify-content: center;
         align-items: center;
         flex-direction: column;
+        box-shadow: 0px 0px 30px 5px silver;
+        padding: 50px;
+        margin-top: 50px;
 `;
-const Row = styled.div`
+const Header = styled.div`
         display: flex;
         align-items: center;
+        width: 100%;
+        justify-content: space-between;
+        font-weight: bold;
+        padding: 15px 0;
+        div{
+            width: 25%;
+            text-align: center;
+        }
+`;
+const Row = styled.div`
+        padding: 15px 0;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        justify-content: space-between;
+        div{
+            width: 25%;
+            text-align: center;
+        }
 `;
 const RowImg = styled.img`
         width: 50px;
         height: 50px;
+`;
+const ShowMoreBtn = styled.button`
+        display: block;
+        margin: 25px auto;
+        padding: 10px 40px;
+        background-color: whitesmoke;
+        border: 1px solid grey;
+        cursor: pointer;
 `;
 function mapStateToProps(state) {
     const { crypto } = state;
